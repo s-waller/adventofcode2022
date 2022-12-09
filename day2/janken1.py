7bc8877779061c778030425c312fe32b1bd53642
s-waller <scott.waller.sw@gmail.com>
2022-12-09 16:39:49 +0000

    added: wrote 2 functions to simplify code, hand_sign and score_increase

--- day2/janken1.py
+++ day2/janken1.py
@@ -3,6 +3,26 @@ import os,sys
 rock_score = 1
 paper_score = 2
 scissors_score = 3
+
+def hand_sign(type):
+    match type:
+        case "A" | "X":
+            return "Rock"
+        case "B" | "Y":
+            return "Paper"
+        case "C" | "Z":
+            return "Scissors"
+
+def score_increase(janken_play):
+    match janken_play:
+        case "Rock":
+            return rock_score
+        case "Paper":
+            return paper_score
+        case "Scissors":
+            return scissors_score
+ 
+
 lose_score = 0
 draw_score = 3
 win_score = 6
@@ -14,32 +34,10 @@ with open(os.path.join(sys.path[0], "input.txt"), "r") as file_content:
     lines = file_content.readlines()
     for i in lines:
         i = str(i).strip()
-        opponent_hand_sign = i[0]
-        match opponent_hand_sign:
-            case "A":
-                opponent_hand_sign = "Rock"
-                opponent_score += rock_score
-            case "B": 
-                opponent_hand_sign = "Paper"
-                opponent_score += paper_score
-            case "C":
-                opponent_hand_sign = "Scissors"
-                opponent_score += scissors_score
-            case _:
-                exit
-        my_hand_sign = i[-1]
-        match my_hand_sign:
-            case "X":
-                my_hand_sign = "Rock"
-                my_score += rock_score
-            case "Y":
-                my_hand_sign = "Paper"
-                my_score += paper_score
-            case "Z":
-                my_hand_sign = "Scissors"
-                my_score += scissors_score
-            case _:
-                exit
+        opponent_hand_sign = hand_sign(i[0])
+        my_hand_sign = hand_sign(i[-1])
+        opponent_score += score_increase(opponent_hand_sign)
+        my_score += score_increase(my_hand_sign)
         
         if opponent_hand_sign == my_hand_sign:
             opponent_score += 3
