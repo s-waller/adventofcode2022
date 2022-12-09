bc54e99faf76a49ef3011cb920cce9f7a663d490
s-waller <scott.waller.sw@gmail.com>
2022-12-09 19:40:04 +0000

    added: created rucksack1.py file for day 3 part 1 challenge, plus wrote pseudocode and added some boilerplate code for reading the input file.

---
+++ day3/rucksack1.py
@@ -0,0 +1,18 @@
+# create dictionary of priorities 
+# 16 (p), 38 (L), 42 (P), 22 (v), 20 (t), and 19 (s)
+#Lowercase item types a through z have priorities 1 through 26.
+#Uppercase item types A through Z have priorities 27 through 52.
+
+#get rucksacks, 1 per line
+with open(os.path.join(sys.path[0], "input.txt"), "r") as file_content:
+    lines = file_content.readlines()
+    for i in lines:
+        i = str(i).strip()
+
+#divide rucksacks into compartments
+
+#find any items that are in both compartments
+
+#get priority of items that were found in both
+
+#total priority
