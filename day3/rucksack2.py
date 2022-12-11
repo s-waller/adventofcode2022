3f08bde07ee4837a590add47961a4c36ae996b8d
s-waller <scott.waller.sw@gmail.com>
2022-12-11 00:17:36 +0000

    changed: modified grouping logic to iterate through 3 lines, perform actions, then empty the group list, repeat.

--- day3/rucksack2.py
+++ day3/rucksack2.py
@@ -5,9 +5,15 @@ priority_total = 0
 with open(os.path.join(sys.path[0], "input.txt"), "r") as file_content:
     lines = file_content.readlines()
     group = []
+    counter = 0
     for i in lines:
         string = str(i).strip()
-        while len(group) < 3:
-            group += string
-            if len(group) == 3:
-                break
+        group.append(string)
+        if len(group) == 3:
+
+            
+
+
+            group = []
+        
+print(len(group))
