3087048f7646d4460189ad93029c07e7d2498065
s-waller <scott.waller.sw@gmail.com>
2022-12-11 00:20:16 +0000

    added: ported over functions 'check_priority' and 'find_common_chars' from rucksack1.py.

--- day3/rucksack2.py
+++ day3/rucksack2.py
@@ -1,5 +1,14 @@
 import os,sys
 
+def check_priority(letter):
+    if letter.islower():
+        return ord(letter) - 96
+    if letter.isupper():
+        return ord(letter) - 38
+
+def find_common_chars(string1,string2):
+    return ''.join(set(string1).intersection(string2))
+
 priority_total = 0
 
 with open(os.path.join(sys.path[0], "input.txt"), "r") as file_content:
