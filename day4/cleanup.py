270bcd6fb688d8ff8d21936e9b1b63257a057a10
s-waller <scott.waller.sw@gmail.com>
2022-12-12 23:39:49 +0000

    added: created logic for checking if either list was a subset of the other. Added running total with print statement to output the answer.

--- day4/cleanup.py
+++ day4/cleanup.py
@@ -11,19 +11,22 @@ def get_elf_zone_range(pair_list,elf_index):
     return pair_list[elf_index].split("-")
 
 def list_elf_zones(elf_zones_shorthand):
-    return list(range(int(elf_zones_shorthand[0]),int(elf_zones_shorthand[-1])))
+    return list(range(int(elf_zones_shorthand[0]),(int(elf_zones_shorthand[-1]) + 1)))
 
+count = 0
 
-for line in (read_file("test.txt")):
+for line in (read_file("input.txt")):
     elf_pair = (str(line).strip())
     pair_list = separate_pair(elf_pair)
 
 # get list of elf1 zones
     elf1_zone_short = get_elf_zone_range(pair_list,0)
     elf1_zones = list_elf_zones(elf1_zone_short)
-    print(elf1_zones)
 
 # get list of elf2 zones
     elf2_zone_short = get_elf_zone_range(pair_list,-1)
     elf2_zones = list_elf_zones(elf2_zone_short)
-    print(elf2_zones)
+
+    if(set(elf1_zones).issubset(set(elf2_zones))) or (set(elf2_zones).issubset(set(elf1_zones))):
+        count += 1
+print(count)
