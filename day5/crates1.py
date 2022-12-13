305780dd41d8c0f179354d3fcf828fb8fb62d4b1
s-waller <scott.waller.sw@gmail.com>
2022-12-13 23:37:54 +0000

    added: created function to parse the starting positions of the crates. Added placeholder for function to be created that converts the starting positions into lists.

--- day5/crates1.py
+++ day5/crates1.py
@@ -1,14 +1,20 @@
-import sys,os
+import sys,os,re
 
 def read_file(file):
     with open(os.path.join(sys.path[0], file), "r") as file_content:
         return file_content.readlines()
 
+def get_starting_positions(crate_info):
+    return re.findall('....?',crate_info)
+
+def create_lists(starting_position_line):
+    return # placeholder
+
 def follow_instructions(move):
-    return #crate_stack_change
+    return # crate_stack_change
 
 for line in (read_file("cratesparsetest.txt")):
     if line.startswith("move"):
         placeholder = 0 # follow instructions
     else:
-        placeholder = 0 # parse crate start position
+        print(get_starting_positions(line)) # parse crate start position
