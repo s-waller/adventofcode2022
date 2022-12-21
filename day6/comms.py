5cd972e4aff17ffe0281c7bf376a3e7e4b062ee2
s-waller <scott.waller.sw@gmail.com>
2022-12-21 20:41:13 +0000

    added: added function to check for unique characters, modifed while loop to loop until unique string found via the added function.

--- day6/comms.py
+++ day6/comms.py
@@ -4,13 +4,22 @@ def read_file(file):
     with open(os.path.join(sys.path[0], file), "r") as file_content:
         return file_content.read()
 
+def unique_characters(string):
+    string_as_set = set(string)
+    if(len(string_as_set) == len(string)):
+        return True
+    return False
+
 content = read_file("input.txt")
 counter = 0
+unique_string_found = False
 
-while counter < (len(content)):
+while unique_string_found == False:
     end_of_slice = counter + 4
     string = content[counter:end_of_slice]
-    
+    if (unique_characters(string)):
+        unique_string_found = True
     counter += 1
 
-    print(string)
+print(end_of_slice)
+print(string)
