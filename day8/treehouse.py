461c570c4aa641a42d9041090b18ecad901a7673
s-waller <scott.waller.sw@gmail.com>
2022-12-27 13:45:19 +0000

    added: wrote boilerplate code for main function and getting the content from the input.txt file.

---
+++ day8/treehouse.py
@@ -0,0 +1,12 @@
+import sys,os
+
+def main():
+    content = read_file("input.txt")
+    print(content)
+
+def read_file(file):
+    with open(os.path.join(sys.path[0], file), "r") as file_content:
+        return file_content.read().split('\n')
+
+if __name__ == "__main__":
+    main()
