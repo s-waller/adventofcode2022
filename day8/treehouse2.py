0835e4904d604f2bf1ca63ec633b876c02e234ed
s-waller <scott.waller.sw@gmail.com>
2022-12-27 23:56:59 +0000

    ignored edge trees since they will provide 0 view distance in 1 direction which when multiplied will result in 0 total. Looped over the rest of the coordinates comparing them the cardinal vars. For the loops, west and north were reveresed since in this program the list needs to be enumerated item by item adn provide an index as a result. Multiplied results of each direction for each tree and update the highest total any time a tree beat it.

--- day8/treehouse2.py
+++ day8/treehouse2.py
@@ -7,24 +7,37 @@ def main():
     list_of_columns = get_columns(content)
     x_count = 0
     y_count = 0
-    visible_trees = 0
-    
+    highest_scenic_score = 0
+
     for row in range_count:
         x_count = 0
         for tree in content[int(row)]:
+            scenic_score = 0
+            east_view = 0
+            west_view = 0
+            north_view = 0
+            south_view = 0
+
             east_side = content[int(row)][int(x_count) + 1:]
             west_side = content[int(row)][:int(x_count)]
             north_side = list_of_columns[int(x_count)][:int(y_count)]
             south_side = list_of_columns[int(x_count)][int(y_count) + 1:]
-            if west_side == [] or east_side == [] or north_side == [] or south_side == []:
-                visible_trees = visible_trees + 1
-            elif tree > max(west_side) or tree > max(east_side) or tree > max(north_side) or tree > max(south_side):
-                visible_trees = visible_trees + 1
 
+            if west_side == [] or east_side == [] or north_side == [] or south_side == []:
+                scenic_score = 0
+            else:
+                east_view = next((x for x, val in enumerate(east_side) if val >= tree), (len(east_side) - 1)) +1
+                west_view = next((x for x, val in enumerate(list(reversed(west_side))) if val >= tree), (len(west_side) - 1)) + 1
+                north_view = next((x for x, val in enumerate(list(reversed(north_side))) if val >= tree), (len(north_side) - 1)) + 1
+                south_view = next((x for x, val in enumerate(south_side) if val >= tree), (len(south_side) - 1)) + 1
+
+            scenic_score = east_view * west_view * north_view * south_view
+            if scenic_score > highest_scenic_score:
+                highest_scenic_score = scenic_score
             x_count = x_count + 1
         y_count = y_count + 1
         
-    print(visible_trees)
+    print(highest_scenic_score)
     
 
 def read_file(file):
