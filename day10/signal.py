98af6980ba7ea8b2565908a37863cf510e58d7e0
s-waller <scott.waller.sw@gmail.com>
2022-12-31 02:08:03 +0000

    added: first draft of day10 part 1.

---
+++ day10/signal.py
@@ -0,0 +1,40 @@
+import sys,os
+
+X = 1
+cycle = 0
+cycle_checkpoints = []
+
+def main():
+    content = read_file("input.txt")
+    for instruction in content:
+        run_instruction(instruction)
+    signal_result = sum(cycle_checkpoints)
+    print(cycle_checkpoints)
+    print(signal_result)
+    print(cycle)
+
+def read_file(file):
+    with open(os.path.join(sys.path[0], file), "r") as file_content:
+        list_of_strings = file_content.read().split('\n')
+        result = [i.split() for i in list_of_strings]
+        return result
+
+def run_instruction(input):
+    global cycle, X
+    if input[0] == 'noop':
+        cycle = cycle + 1
+        cycle_check(cycle)
+
+    elif input[0] == 'addx':
+        cycle = cycle + 1
+        cycle_check(cycle)
+        cycle = cycle + 1
+        X = X + int(input[1])
+        cycle_check(cycle)
+
+def cycle_check(number):
+    if (number == 20) or (( number != 0 ) and ( number % 40 == 0 )):
+        cycle_checkpoints.append(int(X) * int(cycle))
+
+if __name__ == "__main__":
+    main()
