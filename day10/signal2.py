1fd9d980ca025d51e8fc87a90c3cca5692ebbfb3
s-waller <scott.waller.sw@gmail.com>
2022-12-31 13:47:30 +0000

    added: created function to write the output based on row, sprite location and cycle number.

--- day10/signal2.py
+++ day10/signal2.py
@@ -4,6 +4,7 @@ X = 1
 cycle = 1
 cycle_checkpoints = []
 crt_screen = [[],[],[],[],[],[]]
+sprite_position = ['#','#','#']
 
 def main():
     content = read_file("input.txt")
@@ -21,12 +22,15 @@ def read_file(file):
 def run_instruction(input):
     global cycle, X
     if input[0] == 'noop':
+        draw_pixel()
         cycle = cycle + 1
         cycle_check(cycle)
 
     elif input[0] == 'addx':
+        draw_pixel()
         cycle = cycle + 1
         cycle_check(cycle)
+        draw_pixel()
         cycle = cycle + 1
         X = X + int(input[1])
         cycle_check(cycle)
@@ -35,19 +39,26 @@ def cycle_check(number):
     if ((number != 0) and (number % 40 == 0)):
         cycle_checkpoints.append(int(X) * int(cycle))
 
+def draw_pixel():
+    row = check_row()
+    if (row[2] - row[1]) == X or (row[2] - row[1]) == X + 1 or (row[2] - row[1]) == X - 1:
+        crt_screen[row[0]].append('#')
+    else:
+        crt_screen[row[0]].append('.')
+
 def check_row():
     if cycle <= 40:
-        return crt_screen[0]
+        return 0, 0, cycle
     elif cycle >= 41 and cycle <= 80:
-        return crt_screen[1]
+        return 1, 40, cycle
     elif cycle >= 81 and cycle <= 120:
-        return crt_screen[2]
+        return 2, 80, cycle
     elif cycle >= 121 and cycle <= 160:
-        return crt_screen[3]
+        return 3, 120, cycle
     elif cycle >= 161 and cycle <= 200:
-        return crt_screen[4]
+        return 4, 160, cycle
     elif cycle >= 201 and cycle <= 240:
-        return crt_screen[5]
+        return 5, 200, cycle
 
 if __name__ == "__main__":
     main()
