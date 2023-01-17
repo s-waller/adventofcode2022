fe1cee8880d7f57c2eba615007717a95b18f1bdd
s-waller <scott.waller.sw@gmail.com>
2023-01-17 22:35:58 +0000

    added: added functions 'nspect_item' to take actions based on the monkey's inspection, 'choose_monkey_target' to choose which monkey will get an item thrown to them, 'throw_item' to modify the items held by the monkeys and created a for loop for the 20 rounds, for loop for each monkey in each round and a while loop to loop through all the items a monkey is holding until it's holding nothing.

--- day11/monkeys.py
+++ day11/monkeys.py
@@ -3,13 +3,28 @@ import os
 import math
 import collections
 import re
+from pprint import pprint
+import operator
+import time
 
 def main():
     content = read_file("input.txt")
-    print(*content, sep='\n')
-    print(content)
     monkey_data = build_monkey_dict(content)
-    print(monkey_data)
+    pprint(monkey_data, sort_dicts=False)
+
+    for i in range(20): # 20 rounds
+        for monkey in monkey_data:
+            single_monkey = monkey_data.get(monkey)
+            while single_monkey['items']:
+                current_item = int(single_monkey['items'][-1])
+                new_item_value = int(inspect_item(single_monkey, current_item))
+                target = choose_monkey_target(single_monkey, new_item_value)
+                target_monkey_info = monkey_data.get(target)
+                throw_item(single_monkey, target_monkey_info, new_item_value)
+                time.sleep(0.0005)
+                print("\033c")
+                print(monkey)
+                pprint(monkey_data[monkey], sort_dicts=False)
 
 def read_file(file):
     with open(os.path.join(sys.path[0], file), "r") as file_content:
@@ -42,19 +57,34 @@ def build_monkey_dict(input):
             monkey_dict['Monkey' + str(monkey_number[0])]['FalseThrowTarget'] = 'Monkey' + str(number[0])
     return monkey_dict
 
-def test_worry_level(current_worry_level):
-    return math.floor((current_worry_level * 19) / 3)
+def inspect_item(monkey_info, item):
+    operators = {
+        "+": operator.add,
+        "-": operator.sub,
+        "*": operator.mul,
+        "/": operator.truediv,
+        "**": operator.pow
+    }   
+    selected_operator = re.findall('[^\d]', monkey_info['Operation'])[0]
+    try:
+        number = int(re.findall('\d+', monkey_info['Operation'])[0])
+    except:
+        number = 2
+    worry_increase = operators[selected_operator](item, number)
+    result = math.floor(worry_increase / 3)
+    monkey_info['items'][-1] = result
+    return result
 
-def choose_monkey_target(current_worry_level, test_number):
-    if test_worry_level(current_worry_level) % test_number == 0:
-        return # monkey number if true
+def choose_monkey_target(monkey_info, item):
+    test_number = monkey_info['DivideByTestNumber']
+    if item % test_number == 0:
+        return monkey_info['TrueThrowTarget']
     else:
-        return # monkey number if false
+        return monkey_info['FalseThrowTarget']
 
-def choose_monkey():
-    return
-
-def throw_item():
+def throw_item(throwing_monkey, catching_monkey, item):
+    throwing_monkey['items'].pop()
+    catching_monkey['items'].append(item)
     return
 
 if __name__ == "__main__":
