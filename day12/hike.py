0e74719c797de2c2d2839932a3ec0d07c46f41dd
s-waller <scott.waller.sw@gmail.com>
2023-01-30 01:36:08 +0000

    added: create a function 'print_status' to print a visual of the area every move to validate code was functioning.

--- day12/hike.py
+++ day12/hike.py
@@ -2,10 +2,11 @@ import sys
 import os
 import string
 import random
+import time
+import copy
 
 def main():
-    mapped_area = read_file("sample.txt")
-    print(len(mapped_area[0]))
+    mapped_area = read_file("input.txt")
     walked_area = []
     string = ("." * len(mapped_area[0]))
     for i in range(len(mapped_area)):
@@ -17,55 +18,44 @@ def main():
     print(*mapped_area, sep='\n')
     start_location = find_position('S', mapped_area)
     target_location = find_position('E', mapped_area)
-    print(start_location, target_location)
     current_location = current_steps[-1] if current_steps else start_location
     current_steps.append(current_location)
-
-    #walk_paths(mapped_area, options, visited, dfs_traversal)
+    dead_ends = set()
+    walked_area_refresh = copy.deepcopy(walked_area)
 
     while current_location != target_location:
+        print_status(current_steps, dead_ends, target_location, walked_area, walked_area_refresh)
         stepped_ground.add(tuple(current_location))
-        options = scan_directions(current_location, mapped_area, stepped_ground)
+        options = scan_directions(current_location, mapped_area, stepped_ground, dead_ends)
         if options:
             current_location = move(current_location, random.choice(options), current_steps)
-            #current_location = move(current_location, north, current_steps)
         else:
-            dead_end()
+            dead_end(current_location, dead_ends)
+            current_steps = []
+            stepped_ground = set()
+            current_location = start_location
+            current_steps.append(current_location)
     if current_location == target_location:
-            successful_paths.append(current_steps)
-            summit_reached()
+        successful_paths.append(current_steps)
+        summit_reached(current_steps)
+
+def print_status(current_steps, dead_ends, target_location, walked_area, refresh):
+    walked_area = copy.deepcopy(refresh)
+    walked_area[target_location[0]] = walked_area[target_location[0]][:target_location[1]] + "E" + walked_area[target_location[0]][target_location[1] + 1:]
+    for step in current_steps:
+        walked_area[step[0]] = walked_area[step[0]][:step[1]] + "o" + walked_area[step[0]][step[1] + 1:]
+        walked_area[current_steps[-1][0]] = walked_area[current_steps[-1][0]][:current_steps[-1][1]] + "O" + walked_area[current_steps[-1][0]][current_steps[-1][1] + 1:]
+    for nogo in dead_ends:
+        walked_area[nogo[0]] = walked_area[nogo[0]][:nogo[1]] + "X" + walked_area[nogo[0]][nogo[1] + 1:]
+    print("\033c")
+    print(*walked_area, sep='\n')
+    #time.sleep(0.0000000005)
 
 
-"""
-def walk_paths(mapped_area, options, visited, dfs_traversal):
-    for direction in options:
-        
-        walk_paths(mapped_area, options, visited, dfs_traversal)
-
-visited = set()
-dfs_traversal = list()
-
-def walk_paths(graph, options, visited, dfs_traversal):    
-    if direction not in visited:
-        dfs_traversal.append(source)
-        visited.add(source)
-
-        for neighbor_node in graph[source]:
-            dfs(fff, neighbor_node, visited, dfs_traversal)
-
-    return dfs_traversal
-
-
-
-    #for direction in options:
-        #move_selector(current_location, direction)
-"""
-            
 def read_file(file):
     with open(os.path.join(sys.path[0], file), "r") as file_content:
         list_of_strings = file_content.read().split('\n')
-        #result = [list(i) for i in list_of_strings]
-        return list_of_strings #result
+        return list_of_strings
 
 def find_position(input, mapped_area):
     return [[i, location.index(input)]
@@ -101,7 +91,9 @@ def check_elevation(location, mapped_area):
         elevation = 'z'
     return elevation
 
-def valid_coordinates(coordinates, area, visited):
+def valid_coordinates(coordinates, area, visited, dead_ends):
+    if tuple(coordinates) in dead_ends:
+        return False
     if tuple(coordinates) in visited:
         return False
     if coordinates[0] >= len(area) or coordinates[1] >= len(area[0]):
@@ -119,21 +111,23 @@ def can_climb(current_location, target_location, area):
     else:
         return False
 
-def scan_directions(current_location, mapped_area, visited):
+def scan_directions(current_location, mapped_area, visited, dead_ends):
     possible_directions = []
     for direction in (north, south, east, west):
         direction_coordinates = direction(current_location)
-        if valid_coordinates(direction_coordinates, mapped_area, visited):
+        if valid_coordinates(direction_coordinates, mapped_area, visited, dead_ends):
             if can_climb(current_location, direction_coordinates, mapped_area):
                 possible_directions.append(direction)
     return possible_directions
 
-def dead_end():
-    print("Dead End Reached")
+def dead_end(coordinates, dead_ends):
+    dead_ends.add(tuple(coordinates))
+    #print("Dead end reached. Added " + str(tuple(coordinates)) + " to set.")
+    #print("Current dead ends are " + str(dead_ends))
     return 
 
-def summit_reached():
-    print("Summit Reached")
+def summit_reached(current_steps):
+    print("Summit reached in " + str(len(current_steps)) + " steps.")
     return
 
 if __name__ == "__main__":
