e35e3ec232a152b71c6a04a013caf43e0f0f29d6
s-waller <scott.waller.sw@gmail.com>
2023-02-09 00:54:52 +0000

    changed: rewrote search aglo to use breadth first instead of depth.

--- day12/hike.py
+++ day12/hike.py
@@ -4,6 +4,7 @@ import string
 import random
 import time
 import copy
+from collections import deque
 
 def main():
     mapped_area = read_file("input.txt")
@@ -11,51 +12,49 @@ def main():
     string = ("#" * len(mapped_area[0]))
     for i in range(len(mapped_area)):
         walked_area.append(string)
-    successful_paths = []
     current_steps = []
-    stepped_ground = set()
     #print(*mapped_area, sep='\n')
     start_location = find_position('S', mapped_area)
     target_location = find_position('E', mapped_area)
     current_location = current_steps[-1] if current_steps else start_location
     current_steps.append(current_location)
-    dead_ends = set()
     walked_area_refresh = copy.deepcopy(walked_area)
-
-    while current_location != target_location:
-        print_status(current_steps, dead_ends, target_location, walked_area, walked_area_refresh)
-        stepped_ground.add(tuple(current_location))
-        options = scan_directions(current_location, mapped_area, stepped_ground, dead_ends)
-        if options:
-            current_location = move(current_location, random.choice(options), current_steps)
-        else:
-            dead_end(current_location, dead_ends)
-            current_steps.pop()
-            #stepped_ground = set()
-            current_location = current_steps[-1]
-            #current_steps.append(current_location)
-    if current_location == target_location:
-        successful_paths.append(current_steps)
-        summit_reached(current_steps)
-
-def breadth_first_search():
     
-    return
+    print(breadth_first_search(start_location, target_location, mapped_area, walked_area, walked_area_refresh))
 
-def find_shortest_path():
+def breadth_first_search(start, end, mapped_area, walked_area, refresh):
+    queue = deque([start])
+    visited = {start}
+    trace = {}
 
-    shortest_path = 0
-    return shortest_path
+    while queue:
+        print_status(visited, end, walked_area, refresh)
+        vertex = queue.popleft()
+        options = scan_directions(vertex, mapped_area, visited)
+        if vertex == end:
+            break
 
-def print_status(current_steps, dead_ends, target_location, walked_area, refresh):
+        for direction in options:
+            neighbor = direction(vertex)
+            if neighbor not in visited:
+                visited.add(neighbor)
+                trace[neighbor] = vertex
+                queue.append(neighbor)
+
+    path = [end]
+    while path[-1] != start:
+        last_node = path[-1]
+        next_node = trace[last_node]
+        path.append(next_node)
+    
+    return path[::-1]
+
+def print_status(visited, target_location, walked_area, refresh):
     walked_area = copy.deepcopy(refresh)
     walked_area[target_location[0]] = walked_area[target_location[0]][:target_location[1]] + "E" + walked_area[target_location[0]][target_location[1] + 1:]
-    for step in current_steps:
-        walked_area[step[0]] = walked_area[step[0]][:step[1]] + "o" + walked_area[step[0]][step[1] + 1:]
-        walked_area[current_steps[-1][0]] = walked_area[current_steps[-1][0]][:current_steps[-1][1]] + "O" + walked_area[current_steps[-1][0]][current_steps[-1][1] + 1:]
-    for nogo in dead_ends:
-        walked_area[nogo[0]] = walked_area[nogo[0]][:nogo[1]] + "X" + walked_area[nogo[0]][nogo[1] + 1:]
-    
+    for step in visited:
+        walked_area[step[0]] = walked_area[step[0]][:step[1]] + "." + walked_area[step[0]][step[1] + 1:]
+        
     os.system('')
     print(*walked_area, sep='\n')
     #time.sleep(0.005)
@@ -68,24 +67,24 @@ def read_file(file):
         return list_of_strings
 
 def find_position(input, mapped_area):
-    return [[i, location.index(input)]
+    return tuple([[i, location.index(input)]
     for i, location in enumerate(mapped_area)
-    if input in location][0]
+    if input in location][0])
 
 def west(current_location):
-    location = [current_location[0],current_location[1] - 1]
+    location = (current_location[0],current_location[1] - 1)
     return location
 
 def east(current_location):
-    location = [current_location[0],current_location[1] + 1]
+    location = (current_location[0],current_location[1] + 1)
     return location
 
 def north(current_location):
-    location = [current_location[0] - 1,current_location[1]]
+    location = (current_location[0] - 1,current_location[1])
     return location
 
 def south(current_location):
-    location = [current_location[0] + 1,current_location[1]]
+    location = (current_location[0] + 1,current_location[1])
     return location
 
 def move(current_location, next_step, current_steps):
@@ -101,9 +100,7 @@ def check_elevation(location, mapped_area):
         elevation = 'z'
     return elevation
 
-def valid_coordinates(coordinates, area, visited, dead_ends):
-    if tuple(coordinates) in dead_ends:
-        return False
+def valid_coordinates(coordinates, area, visited):
     if tuple(coordinates) in visited:
         return False
     if coordinates[0] >= len(area) or coordinates[1] >= len(area[0]):
@@ -121,19 +118,17 @@ def can_climb(current_location, target_location, area):
     else:
         return False
 
-def scan_directions(current_location, mapped_area, visited, dead_ends):
+def scan_directions(current_location, mapped_area, visited):
     possible_directions = []
     for direction in (north, south, east, west):
         direction_coordinates = direction(current_location)
-        if valid_coordinates(direction_coordinates, mapped_area, visited, dead_ends):
+        if valid_coordinates(direction_coordinates, mapped_area, visited):
             if can_climb(current_location, direction_coordinates, mapped_area):
                 possible_directions.append(direction)
     return possible_directions
 
 def dead_end(coordinates, dead_ends):
     dead_ends.add(tuple(coordinates))
-    #print("Dead end reached. Added " + str(tuple(coordinates)) + " to set.")
-    #print("Current dead ends are " + str(dead_ends))
     return 
 
 def summit_reached(current_steps):
