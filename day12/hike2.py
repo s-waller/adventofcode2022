8996d8c7e68ad2058afa9719815a97b4916de1b5
s-waller <scott.waller.sw@gmail.com>
2023-02-10 00:21:54 +0000

    added: 'hike2.py' to cover part 2. Changed the search to added add all 'a' coordinates as start positions using the saem bfs algo as in part 1.

---
+++ day12/hike2.py
@@ -0,0 +1,139 @@
+import sys
+import os
+import string
+import copy
+from collections import deque
+
+def main():
+    mapped_area = read_file("input.txt")
+    walked_area = []
+    string = ("#" * len(mapped_area[0]))
+    for i in range(len(mapped_area)):
+        walked_area.append(string)
+    current_steps = []
+    #print(*mapped_area, sep='\n')
+    start_location = tuple(find_position('S', mapped_area))[0]
+    other_start_locations = find_position('a', mapped_area)
+    other_start_locations.add(start_location)
+    target_location = tuple(find_position('E', mapped_area))[0]
+    current_location = current_steps[-1] if current_steps else start_location
+    current_steps.append(current_location)
+    walked_area_refresh = copy.deepcopy(walked_area)
+    
+    shortest_path = breadth_first_search(other_start_locations, target_location, mapped_area, walked_area, walked_area_refresh)
+    print(len(shortest_path) - 1)
+
+
+def breadth_first_search(start, end, mapped_area, walked_area, refresh):
+    queue = deque([])
+    visited = set()
+    for i in start:
+        queue.append(i)
+        visited.add(i)
+    
+    trace = {}
+
+    while queue:
+        print_status(visited, end, walked_area, refresh)
+        vertex = queue.popleft()
+        options = scan_directions(vertex, mapped_area, visited)
+        if vertex == end:
+            break
+
+        for direction in options:
+            neighbor = direction(vertex)
+            if neighbor not in visited:
+                visited.add(neighbor)
+                trace[neighbor] = vertex
+                queue.append(neighbor)
+
+    path = [end]
+    while path[-1] not in start:
+        last_node = path[-1]
+        next_node = trace[last_node]
+        path.append(next_node)
+    
+    shortest_path = path[::-1]
+    print_status(visited, end, walked_area, refresh, shortest_path)
+    return shortest_path
+
+def print_status(visited, target_location, walked_area, refresh, shortest_path=[]):
+    walked_area = copy.deepcopy(refresh)
+    walked_area[target_location[0]] = walked_area[target_location[0]][:target_location[1]] + "E" + walked_area[target_location[0]][target_location[1] + 1:]
+    for step in visited:
+        walked_area[step[0]] = walked_area[step[0]][:step[1]] + "." + walked_area[step[0]][step[1] + 1:]
+    if shortest_path:
+        for step in shortest_path:
+            walked_area[step[0]] = walked_area[step[0]][:step[1]] + "@" + walked_area[step[0]][step[1] + 1:]
+        
+    os.system('')
+    print(*walked_area, sep='\n')
+    #print("\033c")
+    print("\033[42F")
+    
+def read_file(file):
+    with open(os.path.join(sys.path[0], file), "r") as file_content:
+        list_of_strings = file_content.read().split('\n')
+        return list_of_strings
+
+def find_position(input, mapped_area):
+    coordinates = set()
+    for i, location in enumerate(mapped_area):
+        for j in enumerate(mapped_area[i]):
+            if input in j:
+                coordinates.add((i, j[0]))
+    return coordinates
+
+def west(current_location):
+    location = (current_location[0],current_location[1] - 1)
+    return location
+
+def east(current_location):
+    location = (current_location[0],current_location[1] + 1)
+    return location
+
+def north(current_location):
+    location = (current_location[0] - 1,current_location[1])
+    return location
+
+def south(current_location):
+    location = (current_location[0] + 1,current_location[1])
+    return location
+
+def check_elevation(location, mapped_area):
+    elevation = mapped_area[location[0]][location[1]]
+    if elevation == 'S':
+        elevation = 'a'
+    if elevation == 'E':
+        elevation = 'z'
+    return elevation
+
+def valid_coordinates(coordinates, area, visited):
+    if tuple(coordinates) in visited:
+        return False
+    if coordinates[0] >= len(area) or coordinates[1] >= len(area[0]):
+        return False
+    if coordinates[0] < 0 or coordinates[1] < 0: # negative values would cause the mover to teleport to the other end of the map
+        return False
+    return True
+
+def can_climb(current_location, target_location, area):
+    elevations = list(string.ascii_lowercase)
+    current_elevation = check_elevation(current_location, area)
+    current_elevations_index = elevations.index(current_elevation)
+    if check_elevation(target_location, area) in elevations[0:(current_elevations_index + 2)]:
+        return True
+    else:
+        return False
+
+def scan_directions(current_location, mapped_area, visited):
+    possible_directions = []
+    for direction in (north, south, east, west):
+        direction_coordinates = direction(current_location)
+        if valid_coordinates(direction_coordinates, mapped_area, visited):
+            if can_climb(current_location, direction_coordinates, mapped_area):
+                possible_directions.append(direction)
+    return possible_directions
+
+if __name__ == "__main__":
+    main()
