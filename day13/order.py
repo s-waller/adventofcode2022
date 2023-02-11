494b2e34224a1b481d394ebc5b8f68fb9cb74cbb
s-waller <scott.waller.sw@gmail.com>
2023-02-11 00:36:16 +0000

    added: modified the 'read_file' function to carry out extra parsing steps (remove empty lines and eval the strings to convert to lists). Due to the changes 'read_file' was renamed to 'parse_file'. Created an extra function 'split_packets' to split the list of parsed data into left and right to make the later comparisons easier.

--- day13/order.py
+++ day13/order.py
@@ -1,15 +1,28 @@
 import sys
 import os
+import ast
 
 def main():
-    left = read_file("input.txt")
-    right = read_file("input.txt")
-    return
+    packets = parse_file("sample.txt")
+    left_and_right_packets = split_packets(packets)
+    left_packets, right_packets = left_and_right_packets[0], left_and_right_packets[1]
 
-def read_file(file):
+
+
+def parse_file(file):
     with open(os.path.join(sys.path[0], file), "r") as file_content:
-        list_of_strings = file_content.read().split('\n')
-        return list_of_strings
-    
+        lines = file_content.read().split('\n')
+
+    lines = [line for i, line in enumerate(lines) if line != '']
+
+    for i, line in enumerate(lines):
+        lines[i] = ast.literal_eval(line)
+    return lines
+
+def split_packets(list_of_packets):
+    left_list = list_of_packets[::2]
+    right_list = list_of_packets[1::2]
+    return left_list, right_list
+
 if __name__ == "__main__":
     main()
